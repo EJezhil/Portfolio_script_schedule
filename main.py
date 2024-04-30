2fd3b9b960b4927acdd98692ec47542dc936c902
Ezhilarasan A <98918113+EJezhil@users.noreply.github.com>
2024-04-30 21:49:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 
-url = 'https://ej-portfolio.onrender.com'
+links = ["https://ej-portfolio.onrender.com","https://ej-todo.onrender.com","https://ej-cafe-api.onrender.com",
+         "https://ej-blog-app.onrender.com","https://ej-ecom-website.onrender.com","https://ej-cafe-finder.onrender.com",
+         "https://ej-stock-api-website.onrender.com","https://ej-image-color-palatte.onrender.com",
+         "https://ej-pdf-to-voice.onrender.com"]
 
-data = requests.get(url= url)
+
+for url in links:
+    data = requests.get(url=url)
